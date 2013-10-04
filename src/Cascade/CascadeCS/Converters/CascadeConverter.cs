ee1192aca1fe6cd18cd34b5675980bcabdf3dcd2
KjosenDay <github1@sibbiheim.com>
2013-10-04 10:42:28 -0700

    Created CascadeConverter and some simple accompanying Converters in CSharp

---
+++ src/Cascade/CascadeCS/Converters/CascadeConverter.cs
@@ -0,0 +1,66 @@
+﻿/*
+* The MIT License (MIT)
+*
+* Copyright (c) 2013 Sibbiheim, LLC.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Sibbiheim.Cascade
+{
+    public sealed class CascadeConverter : IValueConverter
+    {
+        public CascadeConverter()
+        {
+            Converters = new List<IValueConverter>();
+        }
+
+        public List<IValueConverter> Converters { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            object currentValue = value;
+
+            foreach (IValueConverter converter in Converters)
+            {
+                currentValue = converter.Convert(currentValue, targetType, parameter, language);
+            }
+
+            return currentValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            object currentValue = value;
+
+            for (int i = Converters.Count - 1; i >= 0; i--)
+            {
+                currentValue = Converters[i].ConvertBack(currentValue, targetType, parameter, language);
+            }
+
+            return currentValue;
+        }
+    }
+}
